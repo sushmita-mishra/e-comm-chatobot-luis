d99602ac05df0385422d4f9bec6b68428478911c
sushmita-mishra <97576911+sushmita-mishra@users.noreply.github.com>
2022-01-14 12:57:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,6 @@ class DefaultConfig:
     PORT = 3978
     APP_ID = os.environ.get("MicrosoftAppId", "")
     APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    #Create a LUIS App using the JSON provided in data folder. copy the LUIS app URL in LUIS_APP_URL value. 
+    #Follw instructions given in the readme file.
     LUIS_APP_URL = ""
